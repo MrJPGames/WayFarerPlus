cfc227321a3a96b058b32c1c090e50abc7790dba
Jasper W. D. M. Peters <Jasperpeters99@gmail.com>
2019-10-20 18:56:16 +0200

    Add translation button

--- js/mods/review.js
+++ js/mods/review.js
@@ -3,11 +3,17 @@ function modReviewPage(settings){
 	newScript.src = chrome.extension.getURL("pageJs/review.js");
 	document.getElementsByTagName("head")[0].appendChild(newScript);
 
+	var newCss = document.createElement("link");
+	newCss.setAttribute("rel", "stylesheet");
+	newCss.setAttribute("href", chrome.extension.getURL("assets/review.css"));
+	document.getElementsByTagName("head")[0].appendChild(newCss);
+
 	if (settings["revExpireTimer"]){
 		var header = document.getElementsByClassName("niantic-wayfarer-logo")[0];
 		var headerTimer = document.createElement("div");
 		headerTimer.innerText = "Time remaining: ";
-		headerTimer.setAttribute("style", "display: inline-block; margin-left: 5em; color: white;");
+		headerTimer.setAttribute("style", "display: inline-block; margin-left: 5em;");
+		headerTimer.setAttribute("class", "revExprTimer");
 		var timerDiv = document.createElement("div");
 		timerDiv.id = "portalReviewTimer";
 		headerTimer.appendChild(timerDiv);
--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 chrome.storage.local.get("options_set", function (data){
-	if (data["options_set"] == undefined || data["options_set"] < 1){
-		console.log("[WayFarer+] Settings have not been set before. Starting configuration...");
-		setOption("options_set", 3);
-		setOption("darkMode", true);
-		setOption("nomStreetView", true);
-		setOption("revTooCloseWarn", true);
-		setOption("revExpireTimer", true);
-		setOption("nomStats", true);
-		setOption("accPoGo", false);
-		setOption("accIngress", true);
-		console.log("[WayFarer+] Setup finished!");
-	}else if (data["options_set"] < 2){
-		console.log("[WayFarer+] Some new settings seem to be missing. Adding them now...");
-		setOption("options_set", 3);
-		setOption("nomStats", true);
-		setOption("accPoGo", false);
-		setOption("accIngress", true);
-	}else if (data["options_set"] < 3){
-		setOption("options_set", 3);
-		setOption("accPoGo", false);
-		setOption("accIngress", true);
+	var opt_ver = (data["options_set"] != undefined) ? data["options_set"] : 0;
+	switch(opt_ver){
+		case 0:
+			setOption("darkMode", true);
+			setOption("nomStreetView", true);
+			setOption("revTooCloseWarn", true);
+			setOption("revExpireTimer", true);
+		case 1:
+			setOption("nomStats", true);
+		case 2:
+			setOption("accPoGo", false);
+			setOption("accIngress", true);
+		case 3:
+			setOption("revTranslate", true);
+		default:
+			setOption("options_set", 4);
 	}
 });
 
@@ -35,7 +30,7 @@ function init(settings){
 	$(document).ready(function(){
 		var head = document.getElementsByTagName("head")[0];
 		var setInject = document.createElement("script");
-		setInject.innerText = "var settings = " + JSON.stringify(settings) + ";";
+		setInject.innerText = "var settings = " + JSON.stringify(settings) + "; var extURL = \"" + chrome.extension.getURL("") + "\";";
 		head.appendChild(setInject);
 
 		switch (window.location.pathname){
