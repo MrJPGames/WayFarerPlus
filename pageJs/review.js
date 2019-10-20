1dfec4d37cc2d67277e403fcc4a1e1c9976599f7
Jasper W. D. M. Peters <Jasperpeters99@gmail.com>
2019-10-20 18:56:16 +0200

    Add translation button

--- pageJs/review.js
+++ pageJs/review.js
@@ -12,11 +12,47 @@ function setupPage(){
 		//Do page setup
 		if (settings["revExpireTimer"])
 			updateTimer();
-		
 		if (nSubCtrl.reviewType == "NEW"){
 			if (nSubCtrl.pageData.nearbyPortals[0] != undefined)
 				checkNearby();
 		}
+		if (settings["revTranslate"]){
+			addTranslationButtons();
+		}
+	}
+}
+
+function addTranslationButtons(){
+	var elems = document.getElementsByClassName("title-description");
+
+	for (i = 0; i < elems.length; i++){
+		var translateButton = document.createElement("a");
+		translateButton.setAttribute("target", "_BLANK");
+		translateButton.href = "https://translate.google.com/?sl=auto&q=" + encodeURI(elems[i].innerText);
+
+		var translateImage = document.createElement("img");
+		translateImage.src = extURL + "assets/translate.svg";
+		translateImage.setAttribute("class", "translateImg");
+
+		translateButton.appendChild(translateImage);
+
+		elems[i].appendChild(translateButton);
+	}
+
+	if (nSubCtrl.hasSupportingImageOrStatement){
+		var elem = document.getElementsByClassName("supporting-statement-central-field")[0];
+
+		var translateButton = document.createElement("a");
+		translateButton.setAttribute("target", "_BLANK");
+		translateButton.href = "https://translate.google.com/?sl=auto&q=" + encodeURI(elem.innerText);
+
+		var translateImage = document.createElement("img");
+		translateImage.src = extURL + "assets/translate.svg";
+		translateImage.setAttribute("class", "translateImg");
+
+		translateButton.appendChild(translateImage);
+
+		elem.children[0].appendChild(translateButton);
 	}
 }
 
