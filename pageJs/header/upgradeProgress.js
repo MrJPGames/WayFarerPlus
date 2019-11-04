55edd46246037fb50f79ce50f713530a8f335339
Jasper W. D. M. Peters <Jasperpeters99@gmail.com>
2019-11-04 20:21:58 +0100

    Fix errors when user is not logged into wayfarer yet

--- pageJs/header/upgradeProgress.js
+++ pageJs/header/upgradeProgress.js
@@ -1,14 +1,16 @@
 setupHeader();
 function setupHeader(){
     var upgradesProfile = document.getElementById("upgrades-profile-icon");
-    var progress = upgradesProfile.getAttribute("value");
+    if (upgradesProfile != undefined){
+	    var progress = upgradesProfile.getAttribute("value");
 
-    var progressElem = document.createElement("div");
-    progressElem.innerText = progress + "%";
-    if (settings["darkMode"])
-    	progressElem.setAttribute("style", "color: white;");
+	    var progressElem = document.createElement("div");
+	    progressElem.innerText = progress + "%";
+	    if (settings["darkMode"])
+	    	progressElem.setAttribute("style", "color: white;");
 
-    var profileElem = document.getElementsByClassName("inner-container")[1];
+	    var profileElem = document.getElementsByClassName("inner-container")[1];
 
-    profileElem.insertBefore(progressElem, profileElem.children[0]);
+	    profileElem.insertBefore(progressElem, profileElem.children[0]);
+	}
 }
