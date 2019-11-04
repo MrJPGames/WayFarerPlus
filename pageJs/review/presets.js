70c0d77ba9b07b00bf314479ea4e6a427ccd8494
Jasper W. D. M. Peters <Jasperpeters99@gmail.com>
2019-11-04 22:00:56 +0100

    Randomization option for presets added

--- pageJs/review/presets.js
+++ pageJs/review/presets.js
@@ -1,6 +1,8 @@
 var presetContainer; //Needed in multiple functions
 
 function setupPresets(){
+	if (nSubCtrl.reviewType == "EDIT")
+		return; //No edit presets obviously!
 	//Init for first run
 	if (localStorage.wfpPresets == undefined){
 		var defaultPresets = [];
@@ -43,6 +45,8 @@ function addButtonToBar(preset, id){
 	presetButton.setAttribute("class", "presetButton");
 	presetButton.innerText = preset.name;
 	presetButton.onclick = presetClick;
+	if (preset.rng)
+		presetButton.style.fontWeight = "bold";
 	presetContainer.appendChild(presetButton);
 }
 
@@ -50,12 +54,41 @@ function presetClick(e){
 	var pID = e.srcElement.getAttribute("presetID");
 	var presets = JSON.parse(localStorage.wfpPresets);
 	var preset = presets[pID];
-	if (preset.shouldBePortal > 0) document.getElementById(divNames.shouldBePortal).getElementsByClassName("five-stars")[0].children[preset.shouldBePortal-1].click();
-	if (preset.titleAndDescription > 0) document.getElementById(divNames.titleAndDescription).getElementsByClassName("five-stars")[0].children[preset.titleAndDescription-1].click();
-	if (preset.historicOrCultural > 0) document.getElementById(divNames.historicOrCultural).getElementsByClassName("five-stars")[0].children[preset.historicOrCultural-1].click();
-	if (preset.visuallyUnique > 0) document.getElementById(divNames.visuallyUnique).getElementsByClassName("five-stars")[0].children[preset.visuallyUnique-1].click();
-	if (preset.safeAccess > 0) document.getElementById(divNames.safeAccess).getElementsByClassName("five-stars")[0].children[preset.safeAccess-1].click();
-	if (preset.locationAccuracy > 0) document.getElementById(divNames.locationAccuracy).getElementsByClassName("five-stars")[0].children[preset.locationAccuracy-1].click();
+
+
+	var shouldBePortal = preset.shouldBePortal;
+	var titleAndDescription = preset.titleAndDescription;
+	var historicOrCultural = preset.historicOrCultural;
+	var visuallyUnique = preset.visuallyUnique;
+	var safeAccess = preset.safeAccess;
+	var locationAccuracy = preset.locationAccuracy;
+	if (preset.rng){
+		//These are the values we assume can be changed by 1 star up or down and still be "essentially" the same review
+		if (titleAndDescription > 0) titleAndDescription = keepInBounds(parseInt(titleAndDescription)+randomRange(-1,1));
+		if (historicOrCultural > 0) historicOrCultural = keepInBounds(parseInt(historicOrCultural)+randomRange(-1,1));
+		if (visuallyUnique > 0) visuallyUnique = keepInBounds(parseInt(visuallyUnique)+randomRange(-1,1));
+		if (safeAccess > 0) safeAccess = keepInBounds(parseInt(safeAccess)+randomRange(-1,1));
+	}
+
+	if (shouldBePortal > 0) document.getElementById(divNames.shouldBePortal).getElementsByClassName("five-stars")[0].children[shouldBePortal-1].click();
+	if (titleAndDescription > 0) document.getElementById(divNames.titleAndDescription).getElementsByClassName("five-stars")[0].children[titleAndDescription-1].click();
+	if (historicOrCultural > 0) document.getElementById(divNames.historicOrCultural).getElementsByClassName("five-stars")[0].children[historicOrCultural-1].click();
+	if (visuallyUnique > 0) document.getElementById(divNames.visuallyUnique).getElementsByClassName("five-stars")[0].children[visuallyUnique-1].click();
+	if (safeAccess > 0) document.getElementById(divNames.safeAccess).getElementsByClassName("five-stars")[0].children[safeAccess-1].click();
+	if (locationAccuracy > 0) document.getElementById(divNames.locationAccuracy).getElementsByClassName("five-stars")[0].children[locationAccuracy-1].click();
+}
+
+
+function keepInBounds(val){
+	if (val > 5)
+		return 5;
+	if (val < 1)
+		return 1;
+	return val;
+}
+
+function randomRange(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function addPreset(){
@@ -72,6 +105,12 @@ function addPreset(){
 		preset.safeAccess = ansCtrl.formData.safety;
 		preset.locationAccuracy = ansCtrl.formData.location;
 		preset.whatIsIt = whatCtrl.whatNode.id;
+
+		if (confirm("Do you want to have small random mutations applied?\n\n- These should not affect the outcome of the review.\n- These might make it less likely to get a cooldown while using presets.")){
+			preset.rng = true;
+		}else{
+			preset.rng = false;
+		}
 
 		var presets = JSON.parse(localStorage.wfpPresets);
 		presets[presets.length] = preset;
