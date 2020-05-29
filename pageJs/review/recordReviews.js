3d818d4317b6430208402ea3766a7cd88f54892e
tehstone <stone.johnny@gmail.com>
2020-05-28 18:47:46 -0700

    Edits now stored in local storage

--- pageJs/review/recordReviews.js
+++ pageJs/review/recordReviews.js
@@ -1,42 +1,83 @@
 ///Review History
 
-const saveReview = (pageData, submitData) => {
+const saveReview = (nSubCtrl, submitCtrl) => {
+	console.log(submitCtrl);
+	console.log(submitCtrl.formData);
+	//prompt();
 	let toSave = {};
-
 
 	let edit = false;
 	if (nSubCtrl.reviewType !== "NEW") {
 		edit = true;
-	} else {
-	const {
-		title,
-		description,
-		imageUrl,
-		lat,
-		lng,
-		statement,
-		supportingImageUrl,
-	} = pageData;
+		const {
+			titleEdits,
+			descriptionEdits,
+			imageUrl,
+			lat,
+			lng,
+			locationEdits,
+			statement,
+		} = nSubCtrl.pageData;
 
-	toSave = {
-		title,
-		description,
-		imageUrl,
-		lat,
-		lng,
-		statement,
-		supportingImageUrl,
-		ts: +new Date(),
-		review: submitData,
-	};
+		let title = "";
+		let description = "";
+
+		if (nSubCtrl.pageData.titleEdits.length == 0) {
+			title = nSubCtrl.pageData.title;
+		} else {
+			title = submitCtrl.selectedTitleDisplay;
+		}
+
+		if (nSubCtrl.pageData.descriptionEdits.length == 0) {
+			description = nSubCtrl.pageData.description;
+		} else {
+			description = submitCtrl.selectedDescriptionDisplay;
+		}
+
+		toSave = {
+			title: title,
+			titleEdits,
+			description: description,
+			descriptionEdits,
+			imageUrl,
+			lat,
+			lng,
+			selectedLocation: nSubCtrl.selectedEditLocationLatLng,
+			locationEdits,
+			statement,
+			ts: +new Date(),
+		};
+	} else {
+		const {
+			title,
+			description,
+			imageUrl,
+			lat,
+			lng,
+			statement,
+			supportingImageUrl,
+		} = nSubCtrl.pageData;
+
+		toSave = {
+			title,
+			description,
+			imageUrl,
+			lat,
+			lng,
+			statement,
+			supportingImageUrl,
+			ts: +new Date(),
+			review: submitCtrl.formData,
+		};
 	}
 
-	const currentItems = getReviews(edit);
+	const currentItems = getReviews(null, edit);
 
 	const lastItem = currentItems.length
 		? currentItems[currentItems.length - 1]
 		: null;
-	const isSameReview = lastItem && lastItem.imageUrl === imageUrl;
+	
+	const isSameReview = lastItem && lastItem.imageUrl && lastItem.imageUrl === toSave.imageUrl;
 	if (isSameReview) {
 		// update the result
 		currentItems[currentItems.length - 1] = toSave;
@@ -44,11 +85,11 @@ const saveReview = (pageData, submitData) => {
 		// push the new result
 		currentItems.push(toSave);
 	}
-	storeReviewHistory(currentItems, edit);
+	storeReviewHistory(currentItems, null, edit);
 };
 
 document.addEventListener("WFPAllRevHooked", () =>
-	saveReview(nSubCtrl.pageData, false)
+	saveReview(nSubCtrl, false)
 );
 document.addEventListener("WFPAnsCtrlHooked", () => {
 	const {
@@ -60,7 +101,7 @@ document.addEventListener("WFPAnsCtrlHooked", () => {
 
 	ansCtrl.submitForm = function () {
 		// This only works for accepts
-		saveReview(nSubCtrl.pageData, ansCtrl.formData);
+		saveReview(nSubCtrl, ansCtrl);
 		submitForm();
 	};
 
@@ -71,7 +112,7 @@ document.addEventListener("WFPAnsCtrlHooked", () => {
 			const ansCtrl2 = angular.element(ansCtrl2Elem).scope().answerCtrl2;
 			const oldConfirm = ansCtrl2.confirmLowQuality;
 			ansCtrl2.confirmLowQuality = function () {
-				saveReview(nSubCtrl.pageData, {
+				saveReview(nSubCtrl, {
 					...ansCtrl2.formData,
 					review: {
 						...ansCtrl2.formData.review,
@@ -94,7 +135,7 @@ document.addEventListener("WFPAnsCtrlHooked", () => {
 			ansCtrl2.confirmDuplicate = function () {
 				var customFormData = ansCtrl2.formData;
 				customFormData.duplicate = true; //This is because we want to store before we actually let Wayfarer itself set this to true
-				saveReview(nSubCtrl.pageData, {
+				saveReview(nSubCtrl, {
 					...customFormData,
 					duplicateOf: id,
 				}); // duplicateOf is not marked in vm or formData
@@ -104,7 +145,7 @@ document.addEventListener("WFPAnsCtrlHooked", () => {
 	};
 
 	ansCtrl.skipToNext = function () {
-		saveReview(nSubCtrl.pageData, "skipped");
+		saveReview(nSubCtrl, "skipped");
 		skipToNext();
 	};
 });
