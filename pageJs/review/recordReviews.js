b040cccbf8b354d37a9c4515f3288a85c7c35ba4
Jasper W. D. M. Peters <jasperpeters99@gmail.com>
2020-10-30 21:12:23 +0100

    Fix record reviews bug, and potentially image review page bug

--- pageJs/review/recordReviews.js
+++ pageJs/review/recordReviews.js
@@ -1,7 +1,7 @@
 ///Review History
 
 const saveReview = (pageData, submitData) => {
-	if (ansCtrl.reviewType !== "NEW") {
+	if (nSubCtrl.pageData.type !== "NEW") {
 		console.log("Not a new review. Skipping the save.");
 		return;
 	}
@@ -42,56 +42,52 @@ const saveReview = (pageData, submitData) => {
 	storeReviewHistory(currentItems);
 };
 
-document.addEventListener("WFPAllRevHooked", () =>
+document.addEventListener("WFPAllRevHooked", () => {
+	if (ansCtrl.reviewType !== "NEW") {
+		return;
+	}
 	saveReview(nSubCtrl.pageData, false)
-);
-document.addEventListener("WFPAnsCtrlHooked", () => {
-	const {
-		submitForm,
-		skipToNext,
-		showLowQualityModal,
-		markDuplicate,
-	} = ansCtrl;
+
+	const submitForm = ansCtrl.submitForm;
+	const skipToNext = ansCtrl.skipToNext;
+	const showLowQualityModal = nSubCtrl.showLowQualityModal;
+	const markDuplicate = nSubCtrl.markDuplicate;
 
 	ansCtrl.submitForm = function (bool) {
 		// This only works for accepts
-		saveReview(nSubCtrl.pageData, ansCtrl.formData);
+		saveReview(nSubCtrl.pageData, nSubDS.getReviewSubmissionFormData());
 		submitForm(bool);
 	};
 
-	ansCtrl.showLowQualityModal = function () {
+	nSubCtrl.showLowQualityModal = function () {
 		showLowQualityModal();
 		setTimeout(() => {
 			const ansCtrl2Elem = document.getElementById("low-quality-modal");
-			const ansCtrl2 = angular.element(ansCtrl2Elem).scope().answerCtrl2;
+			const ansCtrl2 = angular.element(ansCtrl2Elem).scope().$ctrl;
 			const oldConfirm = ansCtrl2.confirmLowQuality;
 			ansCtrl2.confirmLowQuality = function () {
 				saveReview(nSubCtrl.pageData, {
 					...ansCtrl2.formData,
 					review: {
-						...ansCtrl2.formData.review,
-						comment: ansCtrl2.rejectComment,
-					},
+						...nSubDS.getReviewSubmissionFormData()
+					}
 				});
 				oldConfirm();
 			};
 		}, 10);
 	};
 
-	ansCtrl.markDuplicate = function (id) {
+	nSubCtrl.markDuplicate = function (id) {
 		markDuplicate(id);
 		setTimeout(() => {
-			const ansCtrl2Elem = document.querySelector(
-				".modal-content > [ng-controller]"
-			);
-			const ansCtrl2 = angular.element(ansCtrl2Elem).scope().answerCtrl2;
+			const ansCtrl2Elem = document.getElementsByClassName("modal-content")[0].children[0];
+			const ansCtrl2 = angular.element(ansCtrl2Elem).scope().$ctrl;
 			const confirmDuplicate = ansCtrl2.confirmDuplicate;
 			ansCtrl2.confirmDuplicate = function () {
 				var customFormData = ansCtrl2.formData;
 				customFormData.duplicate = true; //This is because we want to store before we actually let Wayfarer itself set this to true
 				saveReview(nSubCtrl.pageData, {
-					...customFormData,
-					duplicateOf: id,
+					...nSubDS.getReviewSubmissionFormData()
 				}); // duplicateOf is not marked in vm or formData
 				confirmDuplicate();
 			};
