023d5c94b8efddd8fda412b0bf52e70639b82da4
Smooklu <37220586+Smooklu@users.noreply.github.com>
2021-05-06 15:19:02 -0500

    Update wayfarerRating.js

--- pageJs/header/wayfarerRating.js
+++ pageJs/header/wayfarerRating.js
@@ -1,23 +1,26 @@
-function WayfarerRating() {
-        var request = new XMLHttpRequest();
-        request.addEventListener("load", function(evt){
-            if (request.status == 200) {
-                console.log('Profile loaded!')
-                var rating = document.createElement('section');
-                rating.innerText = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].innerText;
-                rating.className = 'rating-bar__segment rating-bar__segment--active'
-                rating.style = 'border-radius:20px 20px 20px 20px; text-transform:capitalize; margin-right:10px;'
-                rating.title = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].getAttribute('uib-tooltip')
-                var profilepic = document.getElementsByClassName('inner-container')[1].children[0]
-                document.body.children[0].children[2].insertBefore(rating,profilepic)
+function GetWayfarerRating() {
+        window.profilerequest = new XMLHttpRequest();
+        window.profilerequest.addEventListener("load", function(evt){
+            if (window.profilerequest.status == 200) {
+                window.rating = window.profilerequest.response.body.getElementsByClassName('rating-bar__segment--active')[0].innerText;
+                InjectRatingIntoHeader();
             }
             else{
                 console.log("Couldn't load profile!")
             }
         }, false);
 
-        request.open('GET', 'profile', true)
-        request.responseType = "document"
-        request.send();
+        window.profilerequest.open('GET', 'profile', true)
+        window.profilerequest.responseType = "document"
+        window.profilerequest.send();
     }
-WayfarerRating();
+function InjectRatingIntoHeader() {
+        var ratingelement = document.createElement('section');
+        ratingelement.innerText = window.rating;
+        ratingelement.className = 'rating-bar__segment rating-bar__segment--active'
+        ratingelement.style = 'border-radius:20px 20px 20px 20px; text-transform:capitalize; margin-right:10px;'
+        ratingelement.title = window.profilerequest.response.body.getElementsByClassName('rating-bar__segment--active')[0].getAttribute('uib-tooltip')
+        var profilepic = document.getElementsByClassName('inner-container')[1].children[0]
+        document.body.children[0].children[2].insertBefore(ratingelement,profilepic)
+}
+GetWayfarerRating();
